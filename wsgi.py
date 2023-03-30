49865e08b9b50e365cb44510d4e25b502d81252a
Jordan Kell <kell.jordan.p@gmail.com>
2023-03-29 20:56:42 -0700

    update wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 import sys
 sys.path.insert(0, '/var/www/jordankell.com/housing_data')
 
+activate_this = '/var/www/jordankell.com/housing_data/venv/bin/activate'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
 from main import app as application
